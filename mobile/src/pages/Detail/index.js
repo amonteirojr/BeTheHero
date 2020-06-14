c4d2c70194f942638f9d43390d92f3f6ea17fe1b
Adriano Monteiro Jr <amonteirojr@gmail.com>
2020-06-14 01:31:25 -0300

    Added the description in the Details page and a ScrollView for big descriptions

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -8,6 +8,7 @@ import {
   Text,
   Linking,
   StatusBar,
+  ScrollView,
 } from "react-native";
 
 import logoImg from "../../assets/logo.png";
@@ -58,41 +59,47 @@ export default function Detail() {
           <Feather name="arrow-left" size={28} color="#E02041" />
         </TouchableOpacity>
       </View>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={styles.incident}>
+          <Text style={([styles.incidentProperty], { marginTop: 0 })}>
+            ONG:
+          </Text>
+          <Text style={styles.incidentValue}>
+            {incident.name} de {incident.city}/{incident.uf}
+          </Text>
 
-      <View style={styles.incident}>
-        <Text style={([styles.incidentProperty], { marginTop: 0 })}>ONG:</Text>
-        <Text style={styles.incidentValue}>
-          {incident.name} de {incident.city}/{incident.uf}
-        </Text>
+          <Text style={styles.incidentProperty}>CASO:</Text>
+          <Text style={styles.incidentValue}>{incident.title}</Text>
 
-        <Text style={styles.incidentProperty}>CASO:</Text>
-        <Text style={styles.incidentValue}>{incident.title}</Text>
+          <Text style={styles.incidentProperty}>DESCRIÇÃO:</Text>
+          <Text style={styles.incidentValue}>{incident.description}</Text>
 
-        <Text style={styles.incidentProperty}>VALOR:</Text>
-        <Text style={styles.incidentValue}>
-          {Intl.NumberFormat("pt-BR", {
-            style: "currency",
-            currency: "BRL",
-          }).format(incident.value)}
-        </Text>
-      </View>
-
-      <View style={styles.contactBox}>
-        <Text style={styles.heroTitle}>Salve o dia!</Text>
-        <Text style={styles.heroTitle}>Seja o herói desse caso.</Text>
-
-        <Text style={styles.heroDescription}>Entre em contato:</Text>
-
-        <View style={styles.actions}>
-          <TouchableOpacity style={styles.action} onPress={sendWhatsapp}>
-            <Text style={styles.actionText}>Whatsapp</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity style={styles.action} onPress={sendMail}>
-            <Text style={styles.actionText}>E-mail</Text>
-          </TouchableOpacity>
+          <Text style={styles.incidentProperty}>VALOR:</Text>
+          <Text style={styles.incidentValue}>
+            {Intl.NumberFormat("pt-BR", {
+              style: "currency",
+              currency: "BRL",
+            }).format(incident.value)}
+          </Text>
         </View>
-      </View>
+
+        <View style={styles.contactBox}>
+          <Text style={styles.heroTitle}>Salve o dia!</Text>
+          <Text style={styles.heroTitle}>Seja o herói desse caso.</Text>
+
+          <Text style={styles.heroDescription}>Entre em contato:</Text>
+
+          <View style={styles.actions}>
+            <TouchableOpacity style={styles.action} onPress={sendWhatsapp}>
+              <Text style={styles.actionText}>Whatsapp</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.action} onPress={sendMail}>
+              <Text style={styles.actionText}>E-mail</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ScrollView>
     </View>
   );
 }
